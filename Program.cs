33aba24c9255933a9277e206f732057f6e9c67d3
Jack Carlson <CarlJack215@GoTVDSB.ca>
2020-09-28 13:40:36 -0400

    3

--- Program.cs
+++ Program.cs
@@ -8,8 +8,33 @@ namespace Arrays_Practice
 {
     class Program
     {
+        Random generator = new Random();
         static void Main(string[] args)
         {
+            Random generator = new Random();
+            //string[] names = new string[2];
+
+            //names[0] = "Jack Carlson";
+
+            //names[1] = "Cole Cantwell";
+
+            //for (int i = 0; i < names.Length; i++)
+            //    Console.WriteLine("Item number " + i + ": " + names[i]);
+
+            int randomNum;
+
+            int[] numbers = new int[30];
+            foreach (int i in numbers)
+            {
+                randomNum = generator.Next(0, 101);
+                Console.Write(randomNum + " ");
+                for ()
+            }
+            
+
+
+
+            Console.ReadLine();
         }
     }
 }
